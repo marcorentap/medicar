05b2e7db727f2c83a8320cc7d4d6716020a767c2
Marc Owen <marcowen20@gmail.com>
2022-07-23 22:16:08 +0900

    Added script for remote access of jupyter lab

---
+++ client/scripts/public_jupyterlab.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+# NOTE: Need to install autossh, jupyterlab and add public key to the server
+tmux new-session -d -s tunnel "autossh -R 8888:localhost:8888 root@lab.medicar.marcorentap.com"
+tmux new-session -d -s jupyter "jupyter lab --no-browser --ip 0.0.0.0 --port 8888 --collaborative"
