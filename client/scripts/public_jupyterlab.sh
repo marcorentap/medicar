5e43c91c76f5420fb5a8116f34f37e204ae45c0f
Marc Owen <marcowen20@gmail.com>
2022-07-27 23:42:48 +0900

    Using ssh instead of autossh, kill sessions on run

--- client/scripts/public_jupyterlab.sh
+++ client/scripts/public_jupyterlab.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
-# NOTE: Need to install autossh, jupyterlab and add public key to the server
-tmux new-session -d -s tunnel "autossh -R 8888:localhost:8888 root@lab.medicar.marcorentap.com"
+# NOTE: Need to install jupyterlab and add public key to the server
+tmux kill-session -t tunnel
+tmux kill-session -t jupyter
+tmux new-session -d -s tunnel "ssh -R 8888:localhost:8888 -o ExitOnForwardFailure=yes root@lab.medicar.marcorentap.com"
 tmux new-session -d -s jupyter "jupyter lab --no-browser --ip 0.0.0.0 --port 8888 --collaborative --notebook-dir='~'"
